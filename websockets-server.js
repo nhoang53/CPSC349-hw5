93cd0116f71f5405eda25accde6838de82bb1b31
nhoang53 <he_llo1002@yahoo.com>
2019-10-19 23:50:50 -0700

    /topic

--- websockets-server.js
+++ websockets-server.js
@@ -6,25 +6,43 @@ var ws = new WebSocketServer
   port: port
 });
 var messages = [];  // array to hold message
+var topic = "";
 
 console.log("websockets server started");
 
+// connect to server
 ws.on("connection", function (socket)
 {
   console.log("client connection established");
+
+  if(topic !== ""){
+    socket.send("*** Topic is '" + topic + "'");
+  }
+
   // send out all old messages to each new connection
   messages.forEach(function (msg){
     socket.send(msg);
   });
 
   // register a callback for message events
-  socket.on("message", function (data){
+  socket.on("message", function (data)
+  {
     console.log("message received: " + data);
-    messages.push(data);
-    // send new messages to all users as each new messages comes in
-    ws.clients.forEach(function (clientSocket){
-      clientSocket.send(data);
-    });
-    //socket.send(data);
+
+    var newTopic = data.split(" ");
+    if(newTopic[0] === "/topic")
+    {
+      topic = data.substring(7);
+      ws.clients.forEach(function (clientSocket){
+        clientSocket.send("*** Topic has changed to '" + topic + "'");
+      });
+    }
+    else{
+      messages.push(data);
+      // send new messages to all clicents as each new messages comes in
+      ws.clients.forEach(function (clientSocket){
+        clientSocket.send(data);
+      });
+    }
   });
 });
